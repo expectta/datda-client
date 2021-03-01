b9753b8eea96ac2b122a7d943ccbc0ebbf275643
김지운 <expect.ta@gmail.com>
2021-03-01 15:11:25 +0900

    [CLIENT] FIX 해당 컴포넌트 범용적으로 사용하기 위한 코드 수정
    컴포넌트 범용적으로 사용하기 위한 코드 수정

--- my-app/src/components/ListForm.tsx
+++ my-app/src/components/ListForm.tsx
@@ -1,47 +1,46 @@
 import React from 'react';
-import { Link, RouteComponentProps } from 'react-router-dom';
+import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 import styled from 'styled-components';
 import { notice } from '../assets/testdata';
 import { ListInnerCard } from './Index';
 interface propsType {
   title: string;
-  fristCategory: string;
-  secondCategory: string;
+  fristCategory?: string;
+  secondCategory?: string;
+  location: RouteComponentProps['location'];
+  history: RouteComponentProps['history'];
+  match: RouteComponentProps['match'];
 }
-export default function ListForm({
-  title,
-  fristCategory,
-  secondCategory,
-}: propsType) {
+function ListForm({ title, fristCategory, secondCategory, match }: propsType) {
+  console.log('현재 url', match.path);
   return (
     <Wrap>
-      <ContentCard>
-        <Title>{title}</Title>
-        <CategoryWrap>
-          <CategoryNotice>{fristCategory}</CategoryNotice>
-          <CategoryEvent>{secondCategory}</CategoryEvent>
-          <CategoryYear>년도별 검색</CategoryYear>
-        </CategoryWrap>
-        <CardWrapper>
-          {notice.data.map((notice, index) => {
-            return (
-              <ListInnerCard
-                key={index}
-                noticeId={notice.noticeId}
-                title={notice.title}
-                category={notice.category}
-                createAt={notice.created_at}
-              ></ListInnerCard>
-            );
-          })}
-        </CardWrapper>
-      </ContentCard>
+      <Title>{title}</Title>
+      <CategoryWrap>
+        <CategoryNotice>{fristCategory}</CategoryNotice>
+        <CategoryEvent>{secondCategory}</CategoryEvent>
+        <CategoryYear>년도별 검색</CategoryYear>
+      </CategoryWrap>
+      <CardWrapper>
+        {notice.data.map((notice, index) => {
+          return (
+            <ListInnerCard
+              key={index}
+              noticeId={notice.noticeId}
+              title={notice.title}
+              category={notice.category}
+              createAt={notice.created_at}
+            ></ListInnerCard>
+          );
+        })}
+      </CardWrapper>
       <ButtonWrapper>
-        <WireButton to="/main/notice/write">작성</WireButton>
+        <WireButton to={`${match.path}/write`}>작성</WireButton>
       </ButtonWrapper>
     </Wrap>
   );
 }
+export default withRouter(ListForm);
 const Wrap = styled.div`
   width: 100%;
   height: 100%;
@@ -74,7 +73,7 @@ const CategoryYear = styled.span`
 `;
 const CardWrapper = styled.div`
   width: 98%;
-  height: 89%;
+  height: 94%;
   margin: 0 auto;
   overflow: auto;
 `;
