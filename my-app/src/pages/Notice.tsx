f6a02f7c095f69dcb34318b2b5260b352354737a
김지운 <expect.ta@gmail.com>
2021-03-01 15:26:09 +0900

    [CLIENT] FIX 추가 컴포넌트 작성
    추가 컴포넌트 작성

--- my-app/src/pages/Notice.tsx
+++ my-app/src/pages/Notice.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, Route, Switch } from 'react-router-dom';
-import { ReadForm, ListForm, WriteNotice } from '../components/Index';
+import { Link, Route, Switch, RouteComponentProps } from 'react-router-dom';
+import {
+  ReadForm,
+  ListForm,
+  WriteNotice,
+  WriteForm,
+} from '../components/Index';
 interface propsType {
-  match: any;
+  location: RouteComponentProps['location'];
+  history: RouteComponentProps['history'];
+  match: RouteComponentProps['match'];
 }
 function Notice({ match }: propsType) {
   return (
     <Wrap>
       <Switch>
-        <Route exact path="/main/notice">
+        <Route exact path={`${match.path}`}>
           <ListForm
             title="공지사항"
             fristCategory="공지사항"
             secondCategory="행사"
           ></ListForm>
         </Route>
-        <Route exact path="/main/notice/write" component={WriteNotice} />
-        <Route exact path="/main/notice/post">
+        <Route exact path={`${match.path}/write`}>
+          <WriteForm title="공지사항 작성" type="notice"></WriteForm>
+        </Route>
+        <Route exact path={`${match.path}/post`}>
           <ReadForm title="공지사항"></ReadForm>
         </Route>
       </Switch>
