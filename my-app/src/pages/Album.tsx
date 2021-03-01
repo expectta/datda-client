a1766830fc325a6f2acd7fa2ee8b960d631285dd
김지운 <expect.ta@gmail.com>
2021-03-01 15:20:27 +0900

    [CLIENT] FIX 코드 간결화 작업
    page 이동 관련 코드 간결화 작업 (match를 통한 url 동적 할당)

--- my-app/src/pages/Album.tsx
+++ my-app/src/pages/Album.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
-export default function Album() {
+import {
+  Route,
+  Switch,
+  RouteComponentProps,
+  withRouter,
+} from 'react-router-dom';
+import { CardList, WriteForm } from '../components/Index';
+interface propsType {
+  location: RouteComponentProps['location'];
+  history: RouteComponentProps['history'];
+  match: RouteComponentProps['match'];
+}
+export default function Album({ match }: propsType) {
   return (
     <Wrap>
-      <ContentCard>
-        <Title>앨범</Title>
-      </ContentCard>
+      <Switch>
+        <Route exact path="/main/album">
+          <CardList
+            title="앨범"
+            imageTitle={'종이놀이시간'}
+            createdAt={'2021년 2월 2일'}
+          ></CardList>
+        </Route>
+        <Route exact pasth={`${match.path}/write`}>
+          <WriteForm title="앨범 등록" type="album"></WriteForm>
+        </Route>
+      </Switch>
     </Wrap>
   );
 }
 const Wrap = styled.div`
   width: 100%;
   height: 100%;
-`;
-const ContentCard = styled.div`
-  ${({ theme }) => theme.common.contentCardDiv}
-`;
-const Title = styled.div`
-  ${({ theme }) => theme.common.contentTitle}
 `;
