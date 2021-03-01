942b38e9919e7f83f1f633ee911dbce182436141
김지운 <expect.ta@gmail.com>
2021-03-01 15:28:05 +0900

    [CLIENT] ADD 컴포넌트 추가
    컴포넌트 추가

---
+++ my-app/src/components/WriteForm.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
+import { ImagePostForm, TextAreaForm } from './Index';
+interface propsType {
+  title: string;
+  type: string;
+  location: RouteComponentProps['location'];
+  history: RouteComponentProps['history'];
+  match: RouteComponentProps['match'];
+}
+//object 사용시 key와 value의 type을 지정해야한다.
+interface objectType {
+  [key: string]: JSX.Element;
+}
+function WriteForm({ match, title, type }: propsType) {
+  const PREVIOUS_PAGE = -1;
+  //메뉴별 선택적으로 화면 구성
+  const printContent: objectType = {
+    medicine: <TextAreaForm />,
+    notice: <TextAreaForm />,
+    meal: <ImagePostForm />,
+    indiNotice: <TextAreaForm />,
+    album: <ImagePostForm />,
+  };
+  return (
+    <Wrap>
+      <Title>{title}</Title>
+      <TitleWrapper>
+        <SubTitle>
+          <TitleInput required placeholder="제목"></TitleInput>
+        </SubTitle>
+        <Writer>{'작성자'}</Writer>
+      </TitleWrapper>
+      <Container>{printContent[type]}</Container>
+      <ButtonWrapper>
+        <PostButton to="/main/notice">작성완료</PostButton>
+        <CancleButton onClick={() => history.go(PREVIOUS_PAGE)}>
+          취소
+        </CancleButton>
+      </ButtonWrapper>
+    </Wrap>
+  );
+}
+export default withRouter(WriteForm);
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+const ContentCard = styled.div`
+  ${({ theme }) => theme.common.contentCardDiv}
+`;
+const Title = styled.div`
+  ${({ theme }) => theme.common.contentTitle}
+`;
+const Container = styled.div`
+  width: 95%;
+  height: 88%;
+  display: grid;
+  grid-gap: 1%;
+  padding: 1%;
+  margin: 0 auto;
+  margin-bottom: 7%;
+`;
+const TitleWrapper = styled.div`
+  display: flex;
+  width: 90%;
+  margin: 0 auto;
+`;
+const TitleInput = styled.input`
+  width: 100%;
+  ${({ theme }) => theme.common.defaultInput}
+`;
+const SubTitle = styled.span`
+  flex: 1 auto;
+`;
+const Writer = styled.span`
+  flex: 1 auto;
+  text-align: right;
+`;
+
+const TextBox = styled.textarea`
+  width: 100%;
+  padding: 2%;
+  height: 50%;
+  outline: none;
+  border: 0px;
+  resize: none;
+  ::placeholder {
+    color: #dbdbdb;
+  }
+  margin-top: 2%;
+  border-radius: 15px 15px 15px 15px;
+  box-shadow: 0px 0px 5px #c8c8c8;
+`;
+const ButtonWrapper = styled.div`
+  text-align: center;
+  width: 95%;
+  height: auto;
+  margin: 3%;
+  a {
+    margin: 2%;
+  }
+`;
+const PostButton = styled(Link)`
+  ${({ theme }) => theme.common.defaultButton}
+`;
+const CancleButton = styled.span`
+  ${({ theme }) => theme.common.defaultButton}
+`;
