05b90ae6df94c2aec52b9178dbc316bddcb827c3
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-03-01 19:08:47 +0900

    [CLIENT][ADD]Addchildren, ProfileList, SelectInstitution

--- my-app/src/components/Index.tsx
+++ my-app/src/components/Index.tsx
@@ -25,6 +25,9 @@ import MedicineList from './MedicineList';
 import WriteMedicine from './WriteMedicine';
 import CardList from './CardList';
 import WriteMeal from './WriteMeal';
+import ProfileList from './ProfileList';
+import SelectInstitution from './SelectInstitution';
+import AddChildren from './AddChildren';
 export {
   MainMenu,
   MiniIndiNotice,
@@ -53,4 +56,7 @@ export {
   WriteMedicine,
   CardList,
   WriteMeal,
+  ProfileList,
+  SelectInstitution,
+  AddChildren,
 };
