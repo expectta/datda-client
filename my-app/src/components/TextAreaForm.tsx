8d272d2152f09d1cec8ef52c5f78725ffd65d59b
김지운 <expect.ta@gmail.com>
2021-03-01 15:27:38 +0900

    [CLIENT] ADD 컴포넌트추가
    컴포넌트 추가

---
+++ my-app/src/components/TextAreaForm.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function TextAreaForm() {
+  return (
+    <>
+      {' '}
+      <TextBox placeholder="내용작성"></TextBox>
+    </>
+  );
+}
+const TextBox = styled.textarea`
+  width: 100%;
+  padding: 2%;
+  height: 100%;
+  outline: none;
+  border: 0px;
+  resize: none;
+  ::placeholder {
+    color: #dbdbdb;
+  }
+  border-radius: 15px 15px 15px 15px;
+  box-shadow: 0px 0px 5px #c8c8c8;
+`;
