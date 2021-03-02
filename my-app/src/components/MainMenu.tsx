bd722b678f5e7f16e53f7a32ea40599e7bc31685
김지운 <expect.ta@gmail.com>
2021-03-03 00:39:53 +0900

    [CLIENT] FIX 아이콘 이미지 변경
    아이콘 이미지 변경

--- my-app/src/components/MainMenu.tsx
+++ my-app/src/components/MainMenu.tsx
@@ -7,24 +7,28 @@ export default function MainMenu() {
     <Wrap>
       <MenuWrap>
         <IconWrap to="/main/notice">
-          <IconImage src="../images/nap.png" alt="공지사항"></IconImage>
+          <IconImage src="../images/notice.png" alt="공지사항"></IconImage>
           <Describe>공지사항</Describe>
         </IconWrap>
         <IconWrap to="/main/medicine">
-          <IconImage src="../images/nap.png" alt="공지사항"></IconImage>
+          <IconImage src="../images/medicine.png" alt="공지사항"></IconImage>
           <Describe>투약의뢰서</Describe>
         </IconWrap>
         <IconWrap to="/main/meal">
-          <IconImage src="../images/nap.png" alt="공지사항"></IconImage>
+          <IconImage src="../images/meal-list.png" alt="공지사항"></IconImage>
           <Describe>급식표</Describe>
         </IconWrap>
         <IconWrap to="/main/indi_notice">
-          <IconImage src="../images/nap.png" alt="공지사항"></IconImage>
+          <IconImage src="../images/indi-notice.png" alt="공지사항"></IconImage>
           <Describe>알림장</Describe>
         </IconWrap>
         <IconWrap to="/main/album">
-          <IconImage src="../images/nap.png" alt="공지사항"></IconImage>
+          <IconImage src="../images/album.png" alt="공지사항"></IconImage>
           <Describe>앨범</Describe>
+        </IconWrap>
+        <IconWrap to="/main/bus">
+          <IconImage src="../images/bus.png" alt="공지사항"></IconImage>
+          <Describe>버스</Describe>
         </IconWrap>
       </MenuWrap>
     </Wrap>
@@ -34,6 +38,7 @@ const Wrap = styled.div`
   width: 100%;
   height: 20%;
   border-top: 1px solid #eeeeee;
+  border-bottom: 1px solid #eeeeee;
 `;
 const MenuWrap = styled.div`
   width: 95%;
@@ -48,11 +53,11 @@ const IconWrap = styled(Link)`
   align-self: center;
 `;
 const IconImage = styled.img`
-  width: 90%;
-  display: block;
+  width: 4rem;
   height: auto;
 `;
 
 const Describe = styled.label`
+  margin-top: 7%;
   display: block;
 `;
