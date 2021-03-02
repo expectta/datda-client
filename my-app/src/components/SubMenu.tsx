2fb720ff2f1cbd4582fe9ff89b13e20745f63ea3
김지운 <expect.ta@gmail.com>
2021-03-02 14:53:14 +0900

    [CLIENT] FIX 버튼 디자인 변경
    버튼 디자인 변경

--- my-app/src/components/SubMenu.tsx
+++ my-app/src/components/SubMenu.tsx
@@ -5,14 +5,13 @@ import styled from 'styled-components';
 export default function SubMenu() {
   return (
     <Wrap>
-      <SubMenuBar to="/main/report">버그리포트</SubMenuBar>
-      <SubMenuBar to="/main/education">교육 프로그램</SubMenuBar>
+      <SubMenuBar to="/main/report">기관설정</SubMenuBar>
+      <SubMenuBar to="/main/education">원아 상태관리</SubMenuBar>
     </Wrap>
   );
 }
 const Wrap = styled.div`
   width: 100%;
-  height: 100%;
   text-align: center;
 `;
 
