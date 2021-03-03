a3fb233b67ad5d3555e4f198494153cc5279c574
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-03-03 10:16:22 +0900

    [CLIENT][ADD]TeacherManage

---
+++ my-app/src/components/TeacherManage.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+interface Props {
+  isTeacher: boolean;
+  teachers: Array<Record<string, unknown>>;
+}
+function TeacherManage({ isTeacher, teachers }: Props) {
+  return isTeacher ? (
+    <div id="teacher">
+      {teachers.map((teacher: any) => (
+        <div className="teacherArea">
+          <input type="checkbox"></input>
+          <span className="indiTeacher">{teacher.userName}</span>
+        </div>
+      ))}
+      <div id="teacherButton">
+        <Button>교사 지정</Button>
+        <Button>교사 퇴직</Button>
+      </div>
+    </div>
+  ) : (
+    <div></div>
+  );
+}
+
+export default TeacherManage;
+
+const Button = styled.button`
+  ${({ theme }) => theme.common.defaultButton}
+  margin : 0 2% 0 2%;
+`;
