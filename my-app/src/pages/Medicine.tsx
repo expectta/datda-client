94e0216c429eb53060cd8df2eb36fcd1e5c6a26f
김지운 <expect.ta@gmail.com>
2021-03-03 19:10:20 +0900

    [CLIENT] FIX 하위컴포넌트로 props 전달코드 작성
    FIX 하위컴포넌트로 props 전달코드 작성

--- my-app/src/pages/Medicine.tsx
+++ my-app/src/pages/Medicine.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, Route, Switch, RouteComponentProps } from 'react-router-dom';
+import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
 import { ListForm, TextAreaForm, WriteForm } from '../components/Index';
+
 interface propsType {
-  location: RouteComponentProps['location'];
-  history: RouteComponentProps['history'];
-  match: RouteComponentProps['match'];
+  userInfo: any;
 }
-export default function Medicine({ match }: propsType) {
+export default function Medicine({ userInfo }: propsType) {
+  const urlMatch = useRouteMatch();
   return (
     <Wrap>
       <Switch>
-        <Route exact path={`${match.path}`}>
+        <Route exact path={`${urlMatch.path}`}>
           <ListForm
+            permission={userInfo.permission}
             title="투약의뢰서"
             fristCategory="투약의뢰서"
             secondCategory="투약보고서"
           ></ListForm>
         </Route>
-        <Route exact path={`${match.path}/write`}>
-          <WriteForm title="투약의뢰서 작성" type="medicine"></WriteForm>
+        <Route exact path={`${urlMatch.path}/write`}>
+          <WriteForm
+            userInfo={userInfo}
+            title="투약의뢰서 작성"
+            type="medicine"
+          ></WriteForm>
         </Route>
       </Switch>
     </Wrap>
