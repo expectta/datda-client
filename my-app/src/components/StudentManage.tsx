5f35ba70824ccc515f5ca58f6a349ab10db76636
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-03-05 00:42:47 +0900

    [CLIENT][REFACTOR]기관설정 폼 변경

--- my-app/src/components/StudentManage.tsx
+++ my-app/src/components/StudentManage.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 interface Props {
   isStudent: boolean;
   students: Array<Record<string, unknown>>;
+  classInfo: Array<Record<string, unknown>>;
   setModalMessage: any;
   setModalVisible: any;
 }
@@ -12,31 +13,44 @@ function StudentManage({
   students,
   setModalVisible,
   setModalMessage,
+  classInfo,
 }: Props) {
   const [count, setCount] = useState<number>(students.length);
 
   return isStudent ? (
     <div id="student">
-      <div id="studentButton">
-        <Button>전체 선택</Button>
-        <Button>반 변경</Button>
-        <Button>퇴원 처리</Button>
-      </div>
       <div id="studentInputArea">
+        <div>원아검색</div>
         <input type="text" placeholder="원아를 입력해주세요"></input>
         <Button>검색</Button>
-        <span>총 인원 수 :</span>
-        <span>{count}</span>
-        <span>명</span>
       </div>
       <div className="studentArea">
-        {students.map((student: any) => (
-          <div>
-            <input type="checkbox" value={student.className}></input>
-            <span>{student.className}</span>
-            <span>{student.userName}</span>
+        <div>원아관리</div>
+        <div id="studentSelect">
+          {classInfo.map((classs: any) => (
+            <button>{classs.className}</button>
+          ))}
+        </div>
+        <div id="studentListArea">
+          <div id="studentTitleList">
+            <span>선택</span>
+            <span>원아이름</span>
+            <span>반</span>
           </div>
-        ))}
+          <div id="studentList">
+            {students.map((student: any) => (
+              <div>
+                <input type="checkbox" value={student.className}></input>
+                <span>{student.className}</span>
+                <span>{student.userName}</span>
+              </div>
+            ))}
+          </div>
+          <div id="studentButtonArea">
+            <button>반 변경</button>
+            <button>퇴원</button>
+          </div>
+        </div>
       </div>
     </div>
   ) : (
