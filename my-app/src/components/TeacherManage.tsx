ddf9c64c6192c7471720fb7db598e8ec71134833
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-03-05 00:42:59 +0900

    [CLIENT][REFACTOR]기관설정 폼 변경

--- my-app/src/components/TeacherManage.tsx
+++ my-app/src/components/TeacherManage.tsx
@@ -7,15 +7,47 @@ interface Props {
 function TeacherManage({ isTeacher, teachers }: Props) {
   return isTeacher ? (
     <div id="teacher">
-      {teachers.map((teacher: any) => (
-        <div className="teacherArea">
-          <input type="checkbox" value={teacher.userName}></input>
-          <span className="indiTeacher">{teacher.userName}</span>
+      <div>승인대기중</div>
+      <div id="teacherWaiting">
+        <div id="teacherWaitingTitle">
+          <span>교사</span>
+          <span>시간</span>
         </div>
-      ))}
-      <div id="teacherButton">
-        <Button>교사 지정</Button>
-        <Button>교사 퇴직</Button>
+        <div id="teacherWaitionList">
+          {teachers.map((teacher: any) => (
+            <div>
+              <span>{teacher.userName}</span>
+              <span>{teacher.created_at}</span>
+              <span className="teacherButtonArea1">
+                <button>수락</button>
+                <button>보류</button>
+                <button>거절</button>
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>교사관리</div>
+      <div id="teacherManage">
+        <div id="teacherManageTitle">
+          <span>선택</span>
+          <span>교사</span>
+          <span>반</span>
+        </div>
+        <div id="teacherManageList">
+          {teachers.map((teacher: any) => (
+            <div className="teacherList">
+              <input type="checkbox" value={teacher.userName}></input>
+              <span className="indiTeacher">{teacher.userName}</span>
+              <span>햇님반</span>
+            </div>
+          ))}
+          <div id="teacherButtonArea2">
+            <button>반 변경</button>
+            <button>퇴직</button>
+          </div>
+        </div>
       </div>
     </div>
   ) : (
