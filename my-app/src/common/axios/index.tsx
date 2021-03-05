926eca9594406d31c581cce7fc2ea34633b5d2e1
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-03-06 00:50:21 +0900

    [CLIENT][ADD]isEmail axios

--- my-app/src/common/axios/index.tsx
+++ my-app/src/common/axios/index.tsx
@@ -48,20 +48,27 @@ export async function requestLogin(email: string, password: string) {
   return mainData;
 }
 
-export const isEmail = (email: string) => {
-  axios
+export const isEmail = async (email: string) => {
+  const results = await axios
     .post('https://datda.link/auth/isemail', {
       // axios.post('http://localhost:5000/auth/isemail', {
       email: email,
     })
     .then((res) => {
-      console.log(res.status, res.data);
-      alert('콘솔창에 console.log(res.status, res.data)');
+      if (res.status === 201) {
+        return 'no';
+      } else if (res.status === 200) {
+        console.log(res.data);
+        return 'yes';
+      } else {
+        return 'hmm';
+      }
     })
     .catch((err) => {
       console.log(err);
       alert('콘솔창에 console.log(err)');
     });
+  return results;
 };
 
 export function requestMainData(permission: string) {
