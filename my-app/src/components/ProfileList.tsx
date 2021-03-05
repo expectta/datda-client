e88ece561d05654e82ab3c73db53a052cb27c7c8
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-03-06 00:53:39 +0900

    [CLIENT][REFACTOR]structure and institution option

--- my-app/src/components/ProfileList.tsx
+++ my-app/src/components/ProfileList.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Profile() {
   const [userInfo, setUserInfo] = useState({
     userName: '김철수',
+    permission: 'teacher',
     email: '철수@네이버',
     mobile: '',
     role: '선생님',
@@ -14,45 +15,84 @@ export default function Profile() {
     <Wrap>
       <ContentCard>
         <Title>프로필</Title>
-        <div id="contentsWrap">
-          <div className="profile name">
-            <span>이름 </span>
-            <span>{userInfo.userName}</span>
-          </div>
-          <div className="profile email">
-            <span>이메일 </span>
-            <span>{userInfo.email}</span>
-          </div>
-          <div className="profile mobile">
-            <span>전화번호 </span>
-            <span>{userInfo.mobile}</span>
-          </div>
-          <div className="profile role">
-            <span>저는 </span>
-            <span>{userInfo.role}</span>
-            <span>입니다.</span>
-          </div>
-          <div className="profile institution">
-            <span>기관 </span>
-            <span>{userInfo.institution}</span>
-          </div>
-          <div className="profile childrenName">
-            <span>아이들 </span>
-            {userInfo.childrenName.map((user: string) => (
-              <div>
-                <span>{user}</span>
+        <div id="profile">
+          <div id="profileArea">
+            <div id="flexLeft">
+              <AvatarCard
+                src="../images/defaultAvatar.png"
+                alt="avatar"
+              ></AvatarCard>
+              <div id="profileButton">
+                <Link to="/main/profile/institution">
+                  <Button>기관등록</Button>
+                </Link>
+                <Link to="/main/profile/children">
+                  <Button>아이등록</Button>
+                </Link>
               </div>
-            ))}
+            </div>
+            <div id="contentsWrap">
+              {userInfo.permission !== 'institution' ? (
+                <>
+                  <div className="profile name">
+                    <span className="blue">이름 </span>
+                    <span>{userInfo.userName}</span>
+                  </div>
+                  <div className="profile email">
+                    <span className="blue">이메일 </span>
+                    <span>{userInfo.email}</span>
+                  </div>
+                  <div className="profile mobile">
+                    <span className="blue">전화번호 </span>
+                    <span>{userInfo.mobile}</span>
+                  </div>
+                  <div className="profile role">
+                    <span className="blue">저는 </span>
+                    <span>{userInfo.role}</span>
+                    <span className="blue">입니다.</span>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="profile name">
+                    <span className="blue">기관이름 </span>
+                    <span>{userInfo.institution}</span>
+                  </div>
+                  <div className="profile email">
+                    <span className="blue">이메일 </span>
+                    <span>{userInfo.email}</span>
+                  </div>
+                  <div className="profile mobile">
+                    <span className="blue">전화번호 </span>
+                    <span>{userInfo.mobile}</span>
+                  </div>
+                  <div className="profile role">
+                    <span className="blue">지위 </span>
+                    <span>원장</span>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
-          <div className="buttonArea">
-            <Link to="/main/profile/institution">
-              <Button>기관등록</Button>
-            </Link>
-            <Link to="/main/profile/children">
-              <Button>아이등록</Button>
-            </Link>
-          </div>
-          <div className="buttonArea">
+
+          {userInfo.permission !== 'institution' ? (
+            <>
+              <div className="blue">원아관리</div>
+              <div id="profileChildList">
+                {userInfo.childrenName.map((child) => (
+                  <div>{child}</div>
+                ))}
+              </div>
+              <div className="blue">기관목록</div>
+              <div id="profileInstiList">
+                <div>{userInfo.institution}</div>
+              </div>
+            </>
+          ) : (
+            <div></div>
+          )}
+
+          <div id="complete">
             <Link to="/main">
               <Button>완료</Button>
             </Link>
@@ -65,15 +105,37 @@ export default function Profile() {
 const Wrap = styled.div`
   width: 100%;
   height: 100%;
+
   #contentsWrap {
+    padding-left: 3%;
+    flex: 3 1 auto;
+  }
+  #flexLeft {
+    flex: 1 1 auto;
+  }
+  #profile {
     padding-left: 5%;
   }
   .profile {
-    margin: 1%;
+    margin-bottom: 13%;
   }
-  .buttonArea {
+  #profileArea {
+    display: flex;
+  }
+  #complete {
     text-align: center;
     margin-bottom: 5%;
+  }
+  .blue {
+    color: blue;
+  }
+  #profileChildList {
+    border: solid 1px;
+    border-radius: 10px;
+  }
+  #profileInstiList {
+    border: solid 1px;
+    border-radius: 10px;
   }
 `;
 const ContentCard = styled.div`
@@ -84,5 +146,11 @@ const Title = styled.div`
 `;
 const Button = styled.button`
   ${({ theme }) => theme.common.defaultButton}
-  margin : 0 5% 0 5%;
+  margin : 0 2% 0 2%;
+`;
+
+const AvatarCard = styled.img`
+  overflow: hidden;
+  resize: both;
+  width: 200px;
 `;
