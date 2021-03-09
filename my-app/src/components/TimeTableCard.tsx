4db6d1815f897ea999eeea3f1f9c9205b2596e8a
김지운 <expect.ta@gmail.com>
2021-03-09 22:57:06 +0900

    [CLIENT] FIX 시간표 리스트 작성, 등록 관련 코드 작성
     FIX 시간표 리스트 작성, 등록 관련 코드 작성

--- my-app/src/components/TimeTableCard.tsx
+++ my-app/src/components/TimeTableCard.tsx
@@ -1,23 +1,56 @@
 import react from 'react';
 import styled from 'styled-components';
-
-export default function TimetableCard() {
+interface propType {
+  timetable: any;
+  type: string;
+  handleUpdateTimetable: any;
+}
+export default function TimetableCard({
+  timetable,
+  type,
+  handleUpdateTimetable,
+}: propType) {
+  // input handler
+  const handleOnChange = (event: any, step: any) => {
+    const { name, value, id } = event.target;
+    handleUpdateTimetable(step, name, value);
+  };
   return (
     <>
       <CardWrapper>
         <Container>
           <NumberWrapper>
-            <Number>No.{1}</Number>
+            <Number>No.{timetable.step}</Number>
           </NumberWrapper>
-          <TimeWrapper>
-            <Time>시간</Time>
-            <InputTime></InputTime>
-            <span>~</span>
-            <InputTime></InputTime>
+          <TimeWrapper id={timetable.step}>
+            <Time>시간 :</Time>
+            {type === 'read' ? (
+              <span>{timetable.time}</span>
+            ) : (
+              <>
+                <InputTime
+                  name="time"
+                  onChange={(e) => {
+                    handleOnChange(e, timetable.step);
+                  }}
+                  placeholder={timetable.time}
+                ></InputTime>
+              </>
+            )}
           </TimeWrapper>
           <ContentsWrapper>
-            <Contents>내용</Contents>
-            <InputContents></InputContents>
+            <Contents>내용 :</Contents>
+            {type === 'read' ? (
+              <ContentsText>{timetable.contents}</ContentsText>
+            ) : (
+              <InputContents
+                name="contents"
+                onChange={(e) => {
+                  handleOnChange(e, timetable.step);
+                }}
+                placeholder={timetable.contents}
+              ></InputContents>
+            )}
           </ContentsWrapper>
         </Container>
       </CardWrapper>
@@ -27,41 +60,53 @@ export default function TimetableCard() {
 const Container = styled.div`
   display: flex;
   width: 100%;
-  text-align: center;
+  // text-align: center;
 `;
 const NumberWrapper = styled.span`
   display: flex;
-  flex: 1 auto;
+  flex: 0.2 auto;
   font-size: 1rem;
 `;
 const TimeWrapper = styled.span`
   display: flex;
-  flex: 2 auto;
+  flex: 1 auto;
 `;
 const ContentsWrapper = styled.span`
   display: flex;
-  flex: 5 auto;
+  flex: 2 auto;
 `;
 const Number = styled.span`
   font-size: 1.2rem;
   flex: 1 auto;
 `;
 const InputNumber = styled.input`
-  ${({ theme }) => theme.common.defaultInput}flex: 1 auto;
-`;
-const Time = styled.span`
+  ${({ theme }) => theme.common.defaultInput}
   flex: 1 auto;
 `;
-const InputTime = styled.input`
+const Time = styled.span`
+  text-align: end;
+  align-self: center;
   width: 3rem;
-  ${({ theme }) => theme.common.defaultInput}flex: 1 auto;
+  padding-right: 3%;
+  flex: 1.3 auto;
+`;
+const InputTime = styled.input`
+  width: 7rem;
+  ${({ theme }) => theme.common.defaultInput}
 `;
 const Contents = styled.span`
   flex: 1 auto;
+  width: 5rem;
+  align-self: center;
+
+  text-align: end;
+  padding-right: 3%;
 `;
 const InputContents = styled.input`
-  width: 5rem;
-  ${({ theme }) => theme.common.defaultInput}flex: 2 auto;
+  ${({ theme }) => theme.common.defaultInput}
+  width: 100%;
+
+  flex: 1 auto;
 `;
 
 const CardWrapper = styled.div`
@@ -70,6 +115,10 @@ const CardWrapper = styled.div`
   height: auto;
   margin: 0 auto;
   margin-top: 2%;
+  margin-bottom: 2%;
   padding: 2%;
   display: flex;
+`;
+const ContentsText = styled.span`
+  flex: 1 auto;
 `;
