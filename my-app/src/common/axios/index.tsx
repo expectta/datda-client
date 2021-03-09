b01cbd00db5fdff0a8b9c39f4fda9ab26b5e8504
김지운 <expect.ta@gmail.com>
2021-03-09 22:52:17 +0900

    [CLIENT] FIX 시간표 등록 axios 함수 수정
     FIX 시간표 등록 axios 함수 수정

--- my-app/src/common/axios/index.tsx
+++ my-app/src/common/axios/index.tsx
@@ -129,9 +129,7 @@ export function requestMainData(token?: string) {
 }
 // 승인, 미승인 원아 리스트 요청
 export function requestApproveChild(childId?: number | null) {
-  axios.defaults.headers.common[
-    'authorization'
-  ] = axios.defaults.headers.common['authorization'] = JSON.parse(
+  axios.defaults.headers.common['authorization'] = JSON.parse(
     localStorage.getItem('loginInfo')!,
   ).accessToken;
   const id = childId || null;
@@ -163,20 +161,23 @@ export const getProfile = (): void => {
     }
   });
 };
-
-export const requestUploadTimetable = () => {
-  axios
+//시간표 등록(원장)
+export const requestUploadTimetable = async (timetable: any) => {
+  axios.defaults.headers.common['authorization'] = JSON.parse(
+    localStorage.getItem('loginInfo')!,
+  ).accessToken;
+  const result = await axios
     .post('https://datda.link/institution/timetable', {
-      // timetable: '[{step:1,time:09:00~10:00,contents:수학공부}]'
-      timetable:
-        '[{step:1,time:09:00~10:00,contents:수학공부},{step:2,time:10:00~11:00,contents:영어공부},{step:3,time:11:00~12:00,contents:인생공부},{step:4,time:12:00~13:00,contents:코딩공부1},{step:5,time:13:00~14:00,contents:코딩공부2},{step:6,time:14:00~15:00,contents:코딩공부},{step:7,time:15:00~16:00,contents:코딩공부},{step:8,time:16:00~17:00,contents:코딩공부},{step:9,time:17:00~18:00,contents:코딩공부},{step:10,time:18:00~19:00,contents:코딩공부},{step:11,time:19:00~20:00,contents:코딩공부},{step:12,time:20:00~21:00,contents:코딩공부},{step:13,time:21:00~22:00,contents:코딩공부},{step:14,time:22:00~22:25,contents:코딩공부},{step:15,time:22:25~22:40,contents:코딩공부},{step:16,time:22:40~22:50,contents:코딩공부},{step:17,time:22:50~23:00,contents:코딩공부},{step:18,time:23:00~23:10,contents:코딩공부},{step:19,time:23:10~23:20,contents:코딩공부},{step:20,time:23:20~23:30,contents:코딩공부},{step:21,time:23:30~23:40,contents:코딩공부},{step:22,time:23:40~23:50,contents:코딩공부},{step:23,time:23:50~24:00,contents:코딩공부}]',
+      timetable: `'${JSON.stringify(timetable)}'`,
     })
     .then((res) => {
       console.log(res.status, res.data);
-      alert('콘솔창에 console.log(res.status, res.data)');
+
+      return res.data;
     })
     .catch((err) => {
       console.log(err);
       alert('콘솔창에 console.log(err)');
     });
+  return result;
 };
