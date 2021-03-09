6456ad1d42a28e1b1ffa9f4513975a1ae5be4c7f
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-03-09 13:54:24 +0900

    [CLIENT][FEATURE]guestApprove waiting page

---
+++ my-app/src/pages/GuestApproving.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+export default function GuestApproving() {
+  return (
+    <div>
+      <div>기관에 승인 요청중입니다.</div>
+    </div>
+  );
+}
