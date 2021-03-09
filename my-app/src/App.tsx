4cbec18fdefa2e21134f7457733dc263c7964d5f
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-03-09 13:41:55 +0900

    [CLIENT]nothing to change

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -14,6 +14,7 @@ import {
   Signup,
   UserInfo,
   SignupCommon,
+  GuestWaiting,
 } from './pages/Index';
 import { Modal } from './components/Index';
 import styled, {
@@ -108,6 +109,7 @@ function App() {
               component={UserInfo}
               userPermission={userInfo.permission}
             />
+            <Route path="/waiting" component={GuestWaiting} />
           </>
           <Route path="/userinfo" component={UserInfo} />
         </Switch>
