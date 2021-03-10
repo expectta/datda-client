45f323281f450665f8cc0fe1eb95ccda971e2c62
김지운 <expect.ta@gmail.com>
2021-03-10 10:49:54 +0900

    [CLIENT] contents 준비중 이미지 추가
     contents 준비중 이미지 추가

--- my-app/src/pages/Bus.tsx
+++ my-app/src/pages/Bus.tsx
@@ -1,5 +1,34 @@
 import react from 'react';
-
+import styled from 'styled-components';
 export default function Bus() {
-  return <>공사중입니다</>;
+  const PREVIOUS_PAGE = -1;
+  return (
+    <Wrap>
+      <div>
+        <Loading id="loading" src="../images/outOfService.png" />
+        <ButtonWrapper>
+          <CancleButton onClick={() => history.go(PREVIOUS_PAGE)}>
+            홈으로
+          </CancleButton>
+        </ButtonWrapper>
+      </div>
+    </Wrap>
+  );
 }
+const Loading = styled.img`
+  width: 40vw;
+`;
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  place-content: center;
+`;
+const CancleButton = styled.span`
+  ${({ theme }) => theme.common.defaultButton}
+  width: 100%;
+`;
+const ButtonWrapper = styled.div`
+  text-align: center;
+`;
