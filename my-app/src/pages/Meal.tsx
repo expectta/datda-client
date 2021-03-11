478c9ee385ae30f03e73add17dda6174b6123195
김지운 <expect.ta@gmail.com>
2021-03-11 21:07:26 +0900

    [CLINET] FIX 사용자 입력에대한 핸들러 작성
     FIX 사용자 입력에대한 핸들러 작성

--- my-app/src/pages/Meal.tsx
+++ my-app/src/pages/Meal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { CardList, WriteMeal, WriteForm } from '../components/Index';
@@ -8,6 +8,24 @@ interface propsType {
 }
 export default function Meal({ userInfo }: propsType) {
   const urlMatch = useRouteMatch();
+  const [inputVlaue, setInputValue] = useState({
+    title: '',
+    content: '',
+    category: '',
+  });
+  // 사용자 입력 값 핸들러
+  const handleInputValue = (
+    name: string,
+    content: string,
+    category: string,
+  ) => {
+    console.log(name, ' 제목은?', content, '내용은?', category, '카테고리는?');
+    setInputValue({
+      ...inputVlaue,
+      [name]: content,
+      category: category,
+    });
+  };
   return (
     <Wrap>
       <Switch>
@@ -21,6 +39,8 @@ export default function Meal({ userInfo }: propsType) {
         </Route>
         <Route exact pasth={`${urlMatch.path}/write`}>
           <WriteForm
+            handleInputValue={handleInputValue}
+            inputVlaue={inputVlaue}
             userInfo={userInfo}
             title="급식표 등록"
             type="meal"
