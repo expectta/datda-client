41a1e3e9a6e4e225514cfc891fc1e01b10e282f1
김지운 <expect.ta@gmail.com>
2021-03-13 03:49:51 +0900

    [CLINET] FIX 급식표 axios 연결 및 데이터 랜더링
     FIX 급식표 axios 연결 및 데이터 랜더링

--- my-app/src/pages/Meal.tsx
+++ my-app/src/pages/Meal.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { CardList, WriteMeal, WriteForm } from '../components/Index';
-
+import { requestMealListAndUpload } from '../common/axios';
 interface propsType {
   userInfo: any;
 }
@@ -13,6 +13,9 @@ export default function Meal({ userInfo }: propsType) {
     content: '',
     type: '',
     category: '',
+  });
+  const [mealList, setMealList] = useState({
+    mealList: [],
   });
   // 카테고리에 대한 상태
   const [radioButton, setRadioButton] = useState('');
@@ -33,6 +36,25 @@ export default function Meal({ userInfo }: propsType) {
       category: category,
     });
   };
+  //급식표 초기화 핸들러
+  const handleInitailzeMealList = async (childId?: number) => {
+    console.log('급식표 요청');
+    const result = await requestMealListAndUpload(childId);
+    console.log(result, ' 이미지 결과값');
+    setMealList({
+      mealList: result,
+    });
+  };
+  // 화면 랜더링 시 급식 초기화
+  useEffect(() => {
+    console.log(userInfo, '유저정보');
+    if (userInfo.permission === 'parent') {
+      const childId = userInfo.mainData[userInfo.currentChild].childId;
+      handleInitailzeMealList(childId);
+    } else {
+      handleInitailzeMealList();
+    }
+  }, []);
   return (
     <Wrap>
       <Switch>
@@ -40,8 +62,7 @@ export default function Meal({ userInfo }: propsType) {
           <CardList
             userInfo={userInfo}
             title="급식표"
-            imageTitle={'2월 급식표'}
-            createdAt={'2021년 2월 2일'}
+            content={mealList.mealList}
           ></CardList>
         </Route>
         <Route exact pasth={`${urlMatch.path}/write`}>
